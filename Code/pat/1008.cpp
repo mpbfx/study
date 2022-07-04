660ce01752f0101fd5338847c457b77f341b8a5b
mpbfx <1835886801@qq.com>
2022-07-04 20:49:34 +0800

    third

--- Code/pat/1008.cpp
+++ Code/pat/1008.cpp
@@ -7,4 +7,12 @@ int main(){
   for(int i = 0; i < N; i++){
    cin >> a[i];
   }
+  int lenth = sizeof(a) / sizeof(int);
+  for(int i = lenth - 1; i >= 0; i--){
+    a[(i + M) % lenth] = a[i];
+  }
+  for(int i = 0; i < lenth; i++){
+    cout << a[i] << " ";
+  }
+  return 0;
 }
---
+++ English/day19.md
@@ -0,0 +1,18 @@
+# day19
+---
+z|e
+-|-
+confidential|机密的
+conspicuous|易见的
+contemplate|打算
+contradict|反驳
+contrive|设法做到，图谋
+crude|粗略的，原油
+embark|从事
+entail|使必要，牵涉
+envisage|设想
+erode|逐步削弱
+
+---
+麻油彭是机密的，他不是易见的，我打算反驳这一观点，图谋逐步削弱麻油彭，
+因为麻油彭从事原油行业，牵涉到了我的利益，我设想这会成功。
